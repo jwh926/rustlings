2963374f554f45bcdcd5aa6b1b698695f8503024
wono <tridath3@gmail.com>
2024-04-03 08:11:20 +0000

    primitive types && quiz1 done

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,11 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(qty: u32) -> u32 {
+    let price_of_apple = if qty > 40 { 1 } else { 2 };
+    qty * price_of_apple
+}
 
 // Don't modify this function!
 #[test]
